4d0b0841680ae2b30e91f4880290c0003b148eab
Brian Kelleher <bkelleher4@gmail.com>
2021-12-16 23:37:34 -0700

    stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="page-index contain">
-    <h1>Home</h1>
+    <h2>Articles</h2>
+    
+    <div class="articles-excerpts">
+      <content-excerpt
+        v-for="article in articles"
+        :key="`ae-${article.slug}`"
+        :content="article" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Context } from '@nuxt/types'
+import ContentExcerpt from '@/components/ContentExcerpt.vue'
+export default {
+    components: { ContentExcerpt },
+  async asyncData ({ $content } : Context) {
+    const articles = await $content('articles').limit(5).fetch()
 
-export default Vue.extend({})
+    return { articles }
+  }
+}
 </script>

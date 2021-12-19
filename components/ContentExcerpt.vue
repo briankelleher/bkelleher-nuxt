09d000c9e4bdc859e3c19075b70dfe37dd922dcf
Brian Kelleher <bkelleher4@gmail.com>
2021-12-19 15:26:43 -0700

    better org for content excerpts

---
+++ components/ContentExcerpt.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content-excerpt">
+    <h3>{{ content.title }}</h3>
+    <p>{{ content.description }}</p>
+    <NuxtLink :to="`/articles/${content.slug}`">
+      Read More
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    content: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.content-excerpt {
+    h3 {
+        margin-bottom: 16px;
+    }
+}
+</style>
---
+++ pages/articles/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page-articles" />
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>

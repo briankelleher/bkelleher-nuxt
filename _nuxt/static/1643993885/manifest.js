36707601a280d5225bb1f34b5219e2a291d6cd46
briankelleher <bkelleher4@gmail.com>
2022-02-04 16:58:07 +0000

    Deploying to gh-pages from @ briankelleher/bkelleher-nuxt@f2047f526cad2ef40ee99e7f05ca5e0aecacd719 🚀

---
+++ _nuxt/static/1643993885/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Farticles","\u002Farticles\u002Fcreating-and-hosting-a-simple-discord-bot","\u002Farticles\u002Fdiscord-bot-development-embedded","\u002Farticles\u002Fsimple-js-dark-mode"]})
---
+++ _nuxt/static/1643993885/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643993885",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"59d92d3f"}}}}(null,"\u002F"));

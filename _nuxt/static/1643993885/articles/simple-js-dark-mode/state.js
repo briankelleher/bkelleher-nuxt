b1d4652d4b3dad682ee7c59d7535790df4679825
briankelleher <bkelleher4@gmail.com>
2022-04-01 15:25:24 +0000

    Deploying to gh-pages from @ briankelleher/bkelleher-nuxt@026f71ef759d5833ede42806aa52ddece648ea3b 🚀

--- _nuxt/static/1643993885/articles/simple-js-dark-mode/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643993885",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fsimple-js-dark-mode",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"59d92d3f"}}}}(null));
